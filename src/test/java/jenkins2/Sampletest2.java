04871986e7015d02090414f40289ac566d2d526f
SONALI TIWARI <SONALI TIWARI@LAPTOP-V5S86BSH>
2022-04-14 18:18:38 +0530

    100 commit

--- src/test/java/jenkins2/Sampletest2.java
+++ src/test/java/jenkins2/Sampletest2.java
@@ -8,16 +8,13 @@ public class Sampletest2 {
 		
 		@Test
 		public void myfirstTest() {
-<<<<<<< HEAD
+
 			Reporter.log("i'm happy today",true);
-=======
+
 			Reporter.log("i'm  not happy today",true);
 			Reporter.log("i'm  not happy today",true);
 		}
->>>>>>> branch 'master' of https://github.com/trivedisonali/Sdet33jenkins.git
+
 		}
 
-	
-
-}
 
